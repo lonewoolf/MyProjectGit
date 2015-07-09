6e2d08c21eea7fb6cb971020224be92213bfe346
lonewoolf <abhi6136@gmail.com>
2015-07-08 23:28:58 -0500

    Re did some urls and housekeeping

--- routes/index.js
+++ routes/index.js
@@ -8,22 +8,39 @@ var money2IndiaText="LOADING....";
 var axisRemitNewText;
 var xoomURL = 'https://www.xoom.com/india';
 var axisRemitText=new Array();
-var cheerio = require('cheerio');
+var cheerio = require('cheerio');	
 var request = require('request');
-request(xoomURL, function(err, resp, body) 
+
+var ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/534.57.2 (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2';
+
+var fromCountry = 'United States';
+
+
+
+request({
+        url : xoomURL,
+        headers : {
+            "User-Agent" : ua
+        }
+    }, function(err, resp, body) 
 							{
 							        if (err)
 							            throw err;
 
 									 $ = cheerio.load(body);
 	
-									xoomText=$('#top p').text();
+									xoomText=$('.xcma-fx-rate').text();
 									xoomText=xoomText.replace("Current locked-in exchange rate*","");
 					
 							});
 
 var remit2IndiaURL = 'https://www.remit2india.com/sendmoneytoindia/UnitedStates/index.jsp';
-request(remit2IndiaURL, function(err, resp, body) 
+request({
+        url : remit2IndiaURL,
+        headers : {
+            "User-Agent" : ua
+        }
+    }, function(err, resp, body) 
 								{
 								        if (err)
 								            throw err;
@@ -32,40 +49,55 @@ request(remit2IndiaURL, function(err, resp, body)
 		//console.log($.html())
 										
 										remit2IndiaText=$('.exRate').text()+" INR";
+										
 
 
 									
 });
 
 
-var money2IndiaURL = 'http://www.icicibank.com/nri-banking/money_transfer/exchange-rate/iframe_exchange_rate.html'
-request(money2IndiaURL, function(err, resp, body) 
+var money2IndiaURL = 'https://m2inet.icicibank.co.in/m2iNet/m2iNetLoginForm.jsp?site=m2ilogin'
+request({
+        url : money2IndiaURL,
+        headers : {
+            "User-Agent" : ua
+        }
+    }, function(err, resp, body) 
 								{
 								        if (err)
 								            throw err;
 
 										 $ = cheerio.load(body);
 		
-										money2IndiaText=$('body marquee strong').text();
+										money2IndiaText='1 USD ='+$('.bigfon').text();
 										
- money2IndiaText=money2IndiaText.replace("USA","1 USD");
+ //money2IndiaText=money2IndiaText.replace("USA","1 USD");
 						
 								});
-var axisRemitURL = 'http://www.timesofmoney.com/remittance/axisremit/secure/axisremitExchangeRate.jsp?partnerId=AXISREMIT&uiId=AXISREMIT&defaultMenu=exchangeRate';
-request(axisRemitURL, function(err, resp, body) 
+var axisRemitURL = 'https://axisremit.axisbank.co.in/remittance/showExchangeRates.action';
+request.post({
+        url : axisRemitURL,
+        headers : {
+            "User-Agent" : ua,
+            "content-type": 'application/x-www-form-urlencoded' 
+        },
+        body: require('querystring').stringify(fromCountry)
+    }, function(err, resp, body) 
 								{
 								        if (err)
 								            throw err;
 
 										 $ = cheerio.load(body);
 		
-											$('#dispExchangeRate').each(function() {
-										    $(this).find('td').each(function(index) {
+		console.log($.html())
+		
+										// 	$('#dispExchangeRate').each(function() {
+										//     $(this).find('td').each(function(index) {
 
-										    axisRemitText[index]=$(this).text();
-										        // console.log('index_'+index+" is "+axisRemitText[index]);
-										    });
-										});
+										//     axisRemitText[index]=$(this).text();
+										//         // console.log('index_'+index+" is "+axisRemitText[index]);
+										//     });
+										// });
 						
 
 								});
